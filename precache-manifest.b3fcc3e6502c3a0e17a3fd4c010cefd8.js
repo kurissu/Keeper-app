724099ba757a04dab120c5806dde32ec9191c4c9
kris.percussion@gmail.com <kurissu@Kriss-MacBook-Pro.local>
2023-01-25 16:19:29 +0700

    Updates

---
+++ precache-manifest.b3fcc3e6502c3a0e17a3fd4c010cefd8.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0350f361cdcc82de4066c5e09e9c9c71",
+    "url": "/keeper-app/index.html"
+  },
+  {
+    "revision": "b1b29cf9efb7207dbe7d",
+    "url": "/keeper-app/static/js/2.df0d89d3.chunk.js"
+  },
+  {
+    "revision": "af5fd8c71f0ce023f533",
+    "url": "/keeper-app/static/js/main.a64a8e11.chunk.js"
+  },
+  {
+    "revision": "75af14cf052a362bf940",
+    "url": "/keeper-app/static/js/runtime-main.34dffe55.js"
+  }
+]);
